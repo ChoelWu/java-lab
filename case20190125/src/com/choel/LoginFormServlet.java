5a87d127f5e969b8edc0cae3a09cdeb38f305e06
ChoelWu <38238531+ChoelWu@users.noreply.github.com>
2019-01-26 10:31:31 +0800

    case20190125
    case20190125

--- case20190125/src/com/choel/LoginFormServlet.java
+++ case20190125/src/com/choel/LoginFormServlet.java
@@ -11,17 +11,36 @@ import java.io.PrintWriter;
 @WebServlet(name = "LoginFormServlet")
 public class LoginFormServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        request.setCharacterEncoding("utf-8"); // 解决中文乱码
+        out.println("<html>");
+        out.println("<head><title>display login information</title></head>");
+        out.println("<body>");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(username == null || username.length() == 0 || password == null || password.length() == 0) {
+            out.println("<h2>输入用户名和密码不正确</h2>");
+            out.println("<br><a href='"+ request.getContextPath() +"/servlet/loginForm'>请重新登录</a>"); // getContextPath() 获取当前java WEB应用的名称
+        } else {
+            out.print("<h2>用户输入信息：</h2>");
+            out.println("<strong>用户名：" + username + "</strong><br>");
+            out.println("<strong>密码：" + password + "</strong><br>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+        out.flush();
+        out.close();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;content=utf-8");
+        response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head><title>login form page</title></head>");
         out.println("<body>");
         out.println("<h2>请输入登录信息</h2>");
-        out.println("<form method='post'> action='" + request.getContextPath() + "/servlet/loginForm'>");
+        out.println("<form method='post' action='" + request.getContextPath() + "/servlet/loginForm'>");
         out.println("<table border='1'><tr><td>");
         out.println("<html>");
         out.println("用户名：</td><td>");
@@ -33,7 +52,7 @@ public class LoginFormServlet extends HttpServlet {
         out.println("<td><input type='submit' value='登录'></td></tr>");
         out.print("</table></form>");
         out.print("</body>");
-        out.print("/html");
+        out.print("</html>");
         out.flush();
         out.close();
     }
